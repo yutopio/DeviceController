c810c06602c4cc2b9c9e5c72b2bc7a5e023ca693
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2012-02-08 13:21:27 +0900

    Identifier management through codes
    * Give integer IDs to all identities appearing in codes
    * Manage all of their names in the list
    * Match corresponding instances that point to same entity
    * This can solve preprocessor reference problem

--- Grammar/Grammar.fs
+++ Grammar/Grammar.fs
@@ -10,11 +10,21 @@ let Parse file =
     let stream = new FileStream(file, FileMode.Open, FileAccess.Read)
     let reader = new StreamReader(stream)
 
+    // Actual parse phase.
     let stateSave = Lexer.getState ()
     Lexer.initState ()
     let parsed = Parser.compilationUnit Lexer.token (LexBuffer<char>.FromTextReader(reader))
+    let finalState = Lexer.getState ()
     Lexer.setState stateSave
+    reader.Close()
+
+    // The useful information here is parsed and finalState.
+
 
     // should return
     //   device list
     //   proc list
+
+
+// This is needed to let Lexer to recursively start parsing
+Lexer.recursiveParseEntrypoint <- Parse
--- Grammar/Types.fs
+++ Grammar/Types.fs
@@ -8,9 +8,69 @@ and command =
 and endTime =
     | To of int
     | For of int
-and ident = int * string
-and proc =
-    | ProcDef of ident * ident list * procBody list
+and ident = int
+and invokable(id) =
+    let mutable _defined : bool = false
+    let mutable _id : ident = id
+
+    member this.defined
+        with get() = _defined
+        and set(value) = _defined <- value
+
+    member this.id
+        with get() = _id
+        and set(value) = _id <- value
+and proc(id, parameters, body) =
+    inherit invokable(id)
+
+    let mutable _parameters : ident list = parameters
+    let mutable _body : procBody list = body
+    let mutable _origin : (int * ident) option = None
+    let mutable _deviceBind : (device * device) list = []
+
+    member this.parameters
+        with get() = _parameters
+        and set(value) = _parameters <- value
+
+    member this.body
+        with get() = _body
+        and set(value) = _body <- value
+
+    member this.origin
+        with get() = _origin
+        and set(value) = _origin <- value
+
+    member this.deviceBind
+        with get() = _deviceBind
+        and set(value) = _deviceBind <- value
+and device(id) =
+    inherit invokable(id)
+
+    let mutable _portName : string = null
+    let mutable _baudRate : int = 9600
+    let mutable _parity : int = 0
+    let mutable _dataBits : int = 0
+    let mutable _stopBits : int = 0
+
+    member this.portName
+        with get() = _portName
+        and set(value) = _portName <- value
+
+    member this.baudRate
+        with get() = _baudRate
+        and set(value) = _baudRate <- value
+
+    member this.parity
+        with get() = _parity
+        and set(value) = _parity <- value
+
+    member this.dataBits
+        with get() = _dataBits
+        and set(value) = _dataBits <- value
+
+    member this.stopBits
+        with get() = _stopBits
+        and set(value) = _stopBits <- value
 and expr =
     | Const of literal
     | Add of expr * expr
