7b2b951bdc739fe444644cfdd12bfd2975330c2b
Yuto Takei <y.takei@is.s.u-tokyo.ac.jp>
2012-02-08 12:21:50 +0900

    Preprocessor fix
    * Preproc directives should be parsed not in syntactical but in lexical analyzer
    * Need to add some codes to track identity pointer changes

--- Grammar/Grammar.fs
+++ Grammar/Grammar.fs
@@ -1,9 +1,20 @@
 module Grammar
 
+open System.IO
 open Microsoft.FSharp.Text.Lexing
 open Lexer
 open Parser
 open Types
 
-let Parse reader =
-    Parser.compilationUnit Lexer.token (LexBuffer<char>.FromTextReader(reader))
+let Parse file =
+    let stream = new FileStream(file, FileMode.Open, FileAccess.Read)
+    let reader = new StreamReader(stream)
+
+    let stateSave = Lexer.getState ()
+    Lexer.initState ()
+    let parsed = Parser.compilationUnit Lexer.token (LexBuffer<char>.FromTextReader(reader))
+    Lexer.setState stateSave
+
+    // should return
+    //   device list
+    //   proc list
--- Grammar/Types.fs
+++ Grammar/Types.fs
@@ -4,17 +4,18 @@ type procBody =
     | Time of command list
     | Proc of ident * expr list
 and command =
-    | Command of ident * expr list * (single option * endTime) option
+    | Command of ident * expr list * (int option * endTime) option
 and endTime =
-    | To of single
-    | For of single
-and ident = string
+    | To of int
+    | For of int
+and ident = int * string
 and proc =
     | ProcDef of ident * ident list * procBody list
 and expr =
     | Const of literal
     | Add of expr * expr
 and literal =
-    | Value of string
+    | Value of ident
     | String of string
+    | Int of int
     | Float of single
